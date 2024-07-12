d856eca9f636c091bbbe928352cb65a12ec335e4
thiagorabaioli <tfrabaioli@outlook.com>
2024-07-12 21:05:14 +0100

    adicionando dependências de segurança

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -55,10 +55,10 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (23, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (24, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
 
-INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '123456', '2001-07-25');
-INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '123456', '1987-12-13');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '$2a$10$FUoMlL4Uz3qSoutpujtjxOoVcUq3dYt2R1StNd9EX/BBHym2JUbsy', '2001-07-25');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '$2a$10$FUoMlL4Uz3qSoutpujtjxOoVcUq3dYt2R1StNd9EX/BBHym2JUbsy', '1987-12-13');
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
