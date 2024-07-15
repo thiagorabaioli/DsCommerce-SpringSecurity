b8d5e94635b4d615812d9c7e98172daf6d1a3fb4
thiagorabaioli <tfrabaioli@outlook.com>
2024-07-15 16:33:19 +0100

    OrderDTO, busca de pedido por id Final OrderDTO (limitando acesso apenas ao Admin

--- src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -23,6 +23,7 @@ public class OrderController {
     @Autowired
     private OrderService service;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         OrderDTO dto = service.findById(id);
