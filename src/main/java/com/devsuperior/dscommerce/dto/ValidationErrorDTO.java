333d10c31cd786afbceba5553444414fcd5b98db
thiagorabaioli <tfrabaioli@outlook.com>
2024-07-18 19:07:06 +0100

    Ajustes nas validações

--- src/main/java/com/devsuperior/dscommerce/dto/ValidationErrorDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ValidationErrorDTO.java
@@ -18,6 +18,7 @@ public class ValidationErrorDTO extends CustomErrorDTO {
     }
 
     public void addError(String fieldName, String message) {
+        errors.removeIf(x -> x.getFieldName().equals(fieldName)); // Remove previous error with the same field name
         errors.add(new FieldMessageDTO(fieldName, message));
     }
 }
