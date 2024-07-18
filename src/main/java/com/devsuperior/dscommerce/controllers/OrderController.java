5ae7bcfe31186fa540bbf1495d94877bb6e5c0f0
thiagorabaioli <tfrabaioli@outlook.com>
2024-07-18 18:18:54 +0100

    Adicionando imgUrl

--- src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -17,7 +17,7 @@ public class OrderController {
     @Autowired
     private OrderService service;
 
-    @PreAuthorize("hasAnyRole(('ROLE_ADMIN, ROLE_CLIENT'))")
+    @PreAuthorize("hasRole(('ROLE_CLIENT'))")
     @GetMapping(value = "/{id}")
     public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         OrderDTO dto = service.findById(id);
